354098ccf6d47faecf092bcd5d1e5ec3ddb6fd05
LyricZhao <lyricz@yeah.net>
2020-02-17 22:43:26 +0800

    Finish minimal kernel

--- src/entry.rs
+++ src/entry.rs
@@ -2,5 +2,15 @@ global_asm!(include_str!("boot/entry64.asm"));
 
 #[no_mangle]
 pub extern "C" fn kernel_entry() -> ! {
-    loop {}
+    extern "C" {
+        fn _start();
+        fn boot_stack_top();
+    }
+    println!("Rust OS minimal kernel");
+    println!("-> _start addr    = 0x{:x}", _start as usize);
+    println!("-> boot_stack_top = 0x{:x}", boot_stack_top as usize);
+
+    panic!("** Kernel panic **");
+
+//    loop {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #![feature(asm)]
 #![feature(global_asm)]
 
+#[macro_use]
+mod io;
+
 mod entry;
 mod lang;
 mod sbi;
