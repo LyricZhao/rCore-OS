a51a6bddb0a6555c8eafd8ee6c5c630a75ad2dc2
LyricZhao <lyricz@yeah.net>
2020-02-17 20:25:38 +0800

    Initial commit with a progress of chapter-2-Cqemu

---
+++ src/boot/entry64.asm
@@ -0,0 +1,16 @@
+.section .text.entry
+.globl _start
+
+_start:
+    la sp, boot_stack_top
+    call kernel_main
+
+    .section .bss.stack
+    .align 12
+    .global boot_stack
+
+boot_stack:
+    .space 4096 * 4
+    .global boot_stack_top
+
+boot_stack_top:
